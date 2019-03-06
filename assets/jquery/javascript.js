504e67d774b69a3dbae19aabd6b5602b6372b884
jxmiller5 <jxmiller5@gmail.com>
2019-03-05 17:44:07 -0700

    Added new code to javascript

--- assets/jquery/javascript.js
+++ assets/jquery/javascript.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
-    var targetNumber = Math.floor(Math.random()*80);
+    var targetNumber = Math.floor(Math.random()*120)+19;
     var counter = 0;
-    var numberOptions = [10, 5, 3, 7];
+    var numberOptions = [1,2,3,4,5,6,7,8,9,10,11,12];
     var loses = 0;
     var wins = 0;
     var gamelock = false;
-  var increment = numberOptions[Math.round(Math.random())];
+    var crystal1 = $(".crystal-1");
+    var crystal2= $(".crystal-2");
+    var crystal3 = $(".crystal-3");
+    var crystal4 = $(".crystal-4");
+    
+    var crystalValue = [];
+
+    function newGame(){
+    
+    crystalValue[0] = [Math.floor(Math.random() * (12 - 1 + 1) + 1)];
+    crystalValue[1] = [Math.floor(Math.random() * (12 - 1 + 1) + 1)];
+    crystalValue[2] = [Math.floor(Math.random() * (12 - 1 + 1) + 1)];
+    crystalValue[3] = [Math.floor(Math.random() * (12 - 1 + 1) + 1)];
+
+    crystal1.attr("data-crystalValue", crystalValue[0]);
+    crystal2.attr("data-crystalValue", crystalValue[1]);
+    crystal3.attr("data-crystalValue", crystalValue[2]);
+    crystal4.attr("data-crystalValue", crystalValue[3]);
+    console.log(crystal1);
+    }
+
+    newGame();
+  
     $("#number-to-guess").text(targetNumber);
 
     var audioElement = document.createElement("audio");
@@ -16,23 +38,26 @@ $(document).ready(function(){
         $(".pause-button").on("click", function() {
           audioElement.pause();
         });
-    for (var i = 0; i < numberOptions.length; i++) {
+    //for (var i = 0; i < 4; i++) {
 
         // For each iteration, we will create an imageCrystal
-        var imageCrystal = $("<img>");
+        //var imageCrystal = $("<img>");
     
         //adds class crystal-image
-        imageCrystal.addClass("crystal-image");
+        //imageCrystal.addClass("crystal-image");
     
         // Each imageCrystal will be given a src link to the crystal image
-        imageCrystal.attr("src", "assets/images/images.jpeg")
+        //imageCrystal.attr("src", "assets/images/images.jpeg")
         // Each imageCrystal will be given a data attribute called data-crystalValue.
         // This data attribute will be set equal to the array value.
-        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+        
+        //imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+        
     
         // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-        $("#crystals").append(imageCrystal);
-      }
+       // $("#crystals").append(imageCrystal);
+      //}
     $(".crystal-image").on("click", function() {
         if (gamelock  !== true){
         // Clicking the button triggers an alert message.
@@ -48,9 +73,8 @@ $(document).ready(function(){
     if (counter === targetNumber) {
       alert("You win!");
       wins++
-      targetNumber = Math.floor(Math.random()*80);
-      $("#number-to-guess").text(targetNumber);
-      counter = 0;
+      newGame();
+      reset();
 
 
     }
@@ -58,11 +82,16 @@ $(document).ready(function(){
     else if (counter >= targetNumber) {
       alert("You lose!!");
       loses++
-      targetNumber = Math.floor(Math.random()*80);
-      $("#number-to-guess").text(targetNumber);
-      counter = 0;
+    newGame();
+    reset();
         
 
+    }
+    function reset(){
+      targetNumber = Math.floor(Math.random()*120)+19;
+      $("#number-to-guess").text(targetNumber);
+      counter = 0;
+      //numberOptions = [Math.floor(Math.random()*20),Math.floor(Math.random()*20),Math.floor(Math.random()*20),Math.floor(Math.random()*20)];
     }
   }
 
