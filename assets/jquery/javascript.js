87e44d5e1442c5d3db16ba6c82da3dc6d9614eb9
jxmiller5 <jxmiller5@gmail.com>
2019-03-06 11:54:14 -0700

    added counter to page instead of alert

--- assets/jquery/javascript.js
+++ assets/jquery/javascript.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     var targetNumber = Math.floor(Math.random()*120)+19;
     var counter = 0;
-    var numberOptions = [1,2,3,4,5,6,7,8,9,10,11,12];
     var loses = 0;
     var wins = 0;
     var gamelock = false;
@@ -38,26 +37,7 @@ $(document).ready(function(){
         $(".pause-button").on("click", function() {
           audioElement.pause();
         });
-    //for (var i = 0; i < 4; i++) {
-
-        // For each iteration, we will create an imageCrystal
-        //var imageCrystal = $("<img>");
     
-        //adds class crystal-image
-        //imageCrystal.addClass("crystal-image");
-    
-        // Each imageCrystal will be given a src link to the crystal image
-        //imageCrystal.attr("src", "assets/images/images.jpeg")
-        // Each imageCrystal will be given a data attribute called data-crystalValue.
-        // This data attribute will be set equal to the array value.
-        
-        //imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-        
-    
-        // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-       // $("#crystals").append(imageCrystal);
-      //}
     $(".crystal-image").on("click", function() {
         if (gamelock  !== true){
         // Clicking the button triggers an alert message.
@@ -68,7 +48,8 @@ $(document).ready(function(){
     counter += crystalValue;
 
     // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + counter);
+    $("#counter").text(counter);
+
         
     if (counter === targetNumber) {
       alert("You win!");
@@ -91,7 +72,7 @@ $(document).ready(function(){
       targetNumber = Math.floor(Math.random()*120)+19;
       $("#number-to-guess").text(targetNumber);
       counter = 0;
-      //numberOptions = [Math.floor(Math.random()*20),Math.floor(Math.random()*20),Math.floor(Math.random()*20),Math.floor(Math.random()*20)];
+      
     }
   }
 
